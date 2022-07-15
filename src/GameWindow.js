f2b63453b3231857bd13eb312740ffd951c960cb
Khary Beddoe <89004487+Veniceav@users.noreply.github.com>
2022-07-15 13:37:10 -0400

    replaced declarations with useContext

--- src/GameWindow.js
+++ src/GameWindow.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Box, Flex } from '@chakra-ui/react';
+import { GameDataContext } from './context/GameDataContext';
 import ActionWindow from './components/ActionWindow';
 import Inventory from './components/Inventory';
 
 const GameWindow = () => {
+  const [health, setHealth] = useState(10);
+  const [dps, setDps] = useState(2);
+  const [cells, setCells] = useState(0);
+  const [maxHealth, setMaxHealth] = useState(health);
+  const [level, setLevel] = useState(1);
   return (
-    <Flex h="95%" w="100%" bg="gray" flexWrap="wrap">
-      <ActionWindow />
-      <Inventory />
+    <Flex bg="whiteAlpha.700" h="95%" w="100%" flexWrap="wrap">
+      <GameDataContext.Provider
+        value={{
+          health,
+          setHealth,
+          dps,
+          setDps,
+          cells,
+          setCells,
+          maxHealth,
+          setMaxHealth,
+          level,
+          setLevel,
+        }}
+      >
+        <ActionWindow />
+        <Inventory />
+      </GameDataContext.Provider>
     </Flex>
   );
 };
